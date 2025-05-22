f67bbb203e02e4db527682bdb33b4d84f163442f
Barnabe Martin <barmarti@f6r4s12.clusters.42paris.fr>
2025-05-22 09:59:03 +0200

    all done

--- main.c
+++ main.c
@@ -6,49 +6,30 @@
 /*   By: barmarti <barmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 18:55:31 by barmarti          #+#    #+#             */
-/*   Updated: 2025/05/14 20:08:27 by barmarti         ###   ########.fr       */
+/*   Updated: 2025/05/22 09:52:32 by barmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-void	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str)
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
-}
-
-void	test_gnl(void *content)
-{
-	
-}
-
 int	main(int ac, char **av)
 {
-	int	fd;
+	int		fd;
+	char	*line;
 
-	if (ac != 2)
-	{
-		ft_putstr("Err || check args");
-		return (1);
-	}
 	fd = open(av[1], O_RDONLY);
 	if (fd < 0)
 	{
-		ft_putstr("Err || can't open file");
-		get_next_line(av[1]);
+		printf("Err || can't open");
 		return (1);
 	}
-	else
+	while ((line = get_next_line(fd)))
 	{
-		get_next_line(av[1]);
-		return (0);
+		printf("%s", line);
+		free(line);
 	}
+	free(line);
+	close(fd);
+	return (0);
 }
